14ecd3540c354e92657a588669448c9d5c97c94d
= <=>
2024-10-31 08:32:35 +0900

    solved homework

---
+++ workbook2/src/main/java/kr/hs/sdh/workbook2/service/HamburgerService.java
@@ -0,0 +1,40 @@
+package kr.hs.sdh.workbook2.service;
+
+import kr.hs.sdh.workbook2.entity.Hamburger;
+import kr.hs.sdh.workbook2.repository.HamburgerRepository;
+import kr.hs.sdh.workbook2.repository.LotteriaHamburgerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+
+@Service
+public final class HamburgerService {
+
+    // 햄버거 데이터 저장소
+    private final HamburgerRepository hamburgerRepository;
+
+    // "HamburgerService" 클래스의 생성자
+    public HamburgerService(LotteriaHamburgerRepository hamburgerRepository) {
+        this.hamburgerRepository = hamburgerRepository;
+    }
+
+    public List<Hamburger> findRecommendedHamburgers() {
+      return this.hamburgerRepository.findHamburgers()
+              .stream()
+              .filter(hamburger -> hamburger.isRecommended)
+              .toList();
+    };
+
+
+    // 햄버거 데이터를 조회하고 금액 순서로 정렬하여 반환
+    public List<Hamburger> getHamburgers() {
+        return this.hamburgerRepository.findHamburgers()
+            .stream()
+            .sorted(
+                Comparator.comparing(Hamburger::getPrice)
+            )
+            .toList();
+    }
+
+}
